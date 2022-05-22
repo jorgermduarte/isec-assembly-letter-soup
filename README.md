82e1dafa473667050e6cbc4a6fd5718d852bf55f
Joka <joka867@hotmail.com>
2022-05-22 01:27:40 +0100

    feat: added the game menu logic and now it displays the game board

--- README.md
+++ README.md
@@ -1,53 +1,47 @@
-# assembly-letter-soup
+# Project specifications (Assembly Letter Soup)
 
+- [ ] **GAME MENU**
+   - [x] jogar
+   - [ ] top 10
+   - [x] sair
 
+- [ ] **QUADRADO AUXILIAR**:
+    - [ ] apresentacao das palavras a encontrar num quadrado auxiliar ao tabuleiro
+    - [ ] palavras identificadas no tabuleiro ficam com background diferente
+      - [ ] é eliminada da lista ou fica com o fundo azul.
 
+- [x] **Navegação**:
+    - [x] navegacao teclado ( setas )
+    - [x] . selecao letra tecla enter.
 
-# Project specifications
+- [ ] **BOARD**:
+    - [ ] base de jogo ( tabuleiro 12x12)
+    - [ ] gerado tabuleiro com letras aleatórias
+    - [ ] disposição de letras ( vertical, horizontal, diagonal) todos os sentidos
+        - [ ] sobre este tabuleiro com letras aleatórias é colocado as palavras a encontrar
+        - [ ] as palavras aleatorias devem derivar de um ficheiro de texto com o local e o local a serem colocadas no tabuleiro
+    - [ ] identificação de todas as palavras a descobrir
+    - [ ] ao encontrar uma palavra no tabuleiro o seu fundo deve ficar verde
+    - [ ] identificação de fim de jogo
+        - [ ] o jogo termina quando se esgota o tempo ou quando o utilizador encontra todas as palavras
 
-```
-menu:
-    . jogar
-    . top 10
-    . sair
+- [ ] **Pontuação**:
+    - [ ] segundos despendidos no jogo
+    - [ ] numero de palavras encontradas
+    - [ ] numero de tentativas falhadas
 
-quadrado auxiliar:
-    . apresentacao das palavras a encontrar num quadrado auxiliar ao tabuleiro
-    . palavras identificadas no tabuleiro ficam com background diferente
-        . é eliminada da lista ou fica com o fundo azul.
+- [ ] **Classificação**:
+    - [ ] apresentacao pontuacao obtida
+    - [ ] nome do utilizador e valor classficacao
+    - [ ] leitura de um ficheiro com a classificao dos jogadores
+    - [ ] no fim de jogo, se a classificao obtida estiver no top 10:
+        - [ ] é pedido ao jogador o seu nome de utilizador e é gravado.
 
-board:
-    . base de jogo ( tabuleiro 12x12)
-    . gerado tabuleiro com letras aleatórias
-    . disposição de letras ( vertical, horizontal, diagonal) todos os sentidos
-        . sobre este tabuleiro com letras aleatórias é colocado as palavras a encontrar
-        . as palavras aleatorias devem derivar de um ficheiro de texto com o local e o local a serem colocadas no tabuleiro
-    . identificao de todas as palavras a descobrir
-    . ao encontrar uma palavra no tabuleiro o seu fundo deve ficar verde
-    . identificao fim de jogo
-        . o jogo termina quando se esgota o tempo ou quando o utilizador encontra todas as palavras
+- [ ] **Modo Entrega**:
+    - [ ] plataforma NONIO
+    - [ ] grupo de 2 elementos
+    - [ ] relatorio formado pdf e papel ( máximo de 5 páginas)
 
-navegacao:
-    . navegacao teclado ( setas ) . selecao letra tecla enter.
+- [ ] **Defesa**:
+    - [ ] pedidos de pequenas alteracoes
 
-pontuacao:
-    . segundos despendidos no jogo
-    . numero de palavras encontradas
-    . numero de tentativas falhadas
-
-classificacao:
-    . apresentacao pontuacao obtida
-    . nome do utilizador e valor classficacao
-    . leitura de um ficheiro com a classificao dos jogadores
-    . no fim de jogo, se a classificao obtida estiver no top 10:
-        . é pedido ao jogador o seu nome de utilizador e é gravado.
-
-. modo entrega:
-    . plataforma NONIO
-    . grupo de 2 elementos
-    . relatorio formado pdf e papel ( máximo de 5 páginas)
-
-. defesa:
-    . pedidos de pequenas alteracoes
-
-```
--- trabalho.asm
+++ trabalho.asm
@@ -25,9 +25,19 @@ dseg  segment para public 'data' ; start of code segment D
 				db "                                                          ",13,10
 				db "                                                          ",13,10,'$'
 
+		Erro_Open       db      'Erro ao tentar abrir o ficheiro$'
+        Erro_Ler_Msg    db      'Erro ao tentar ler do ficheiro$'
+        Erro_Close      db      'Erro ao tentar fechar o ficheiro$'
+        Fich         	db      'DADOS.TXT',0
+        HandleFich      dw      0
+        car_fich        db      ?
+
+		Car			db	32	; Guarda um caracter do Ecra
+		Cor			db	7	; Guarda os atributos de cor do caracter
+		POSy		db	1	; a linha pode ir de [1 .. 25]
+		POSx		db	2	; POSx pode ir [1..80]
 dseg	ends ; end of code segment D
 
-; ================ MAIN PROCEDURES =================
 cseg	segment para public 'code' ; start of code segment C
 
 assume cs:cseg, ds:dseg, ss:pilha
@@ -67,17 +77,169 @@ DisplayMenu	proc
 DisplayMenu	endp
 
 ReadKeyboardInput	PROC
-		mov	ah,08h
-		int	21h
-		mov	ah,0
-		cmp	al,0
-		jne	leavekey
-		mov	ah, 08h
-		int	21h
-		mov	ah,1
-leavekey:
-	ret
+	mov	ah,08h
+	int	21h
+	mov	ah,0
+	cmp	al,0
+	jne	leavekey
+	mov	ah, 08h
+	int	21h
+	mov	ah,1
+	leavekey:
+		ret
 ReadKeyboardInput	endp
+
+DisplayFile	proc
+	mov     ah,3dh
+	mov     al,0
+	lea     dx,Fich
+	int     21h
+	jc      erro_abrir
+	mov     HandleFich,ax
+	jmp     ler_ciclo
+	erro_abrir:
+			mov     ah,09h
+			lea     dx,Erro_Open
+			int     21h
+			jmp     sai
+
+	ler_ciclo:
+			mov     ah,3fh
+			mov     bx,HandleFich
+			mov     cx,1
+			lea     dx,car_fich
+			int     21h
+			jc		erro_ler
+			cmp		ax,0		;EOF?
+			je		fecha_ficheiro
+			mov     ah,02h
+			mov		dl,car_fich
+			int		21h
+			jmp		ler_ciclo
+
+	erro_ler:
+			mov     ah,09h
+			lea     dx,Erro_Ler_Msg
+			int     21h
+
+	fecha_ficheiro:
+			mov     ah,3eh
+			mov     bx,HandleFich
+			int     21h
+			jnc     sai
+
+			mov     ah,09h
+			lea     dx,Erro_Close
+			Int     21h
+	sai:
+		ret
+DisplayFile	endp
+
+;todo generate a new board with random letters
+GenerateNewGameBoard proc
+GenerateNewGameBoard endp
+
+;set a game word or words based on the file and word position
+SetBoardGameWord proc
+SetBoardGameWord endp
+
+;displays the game words on the board
+DisplayGameWords proc
+DisplayGameWords endp
+
+HandleWordSelection	PROC
+	CICLO:	
+			; goto_xy	POSxa,POSya	; Vai para a posição anterior do cursor
+			; mov		ah, 02h
+			; mov		dl, Car	; Repoe Caracter guardado 
+			; int		21H		
+
+			goto_xy	POSx,POSy	; Vai para nova posição
+			mov 	ah, 08h
+			mov		bh,0		; numero da página
+			int		10h		
+			mov		Car, al		; Guarda o Caracter que está na posição do Cursor
+			mov		Cor, ah		; Guarda a cor que está na posição do Cursor
+			
+			goto_xy	78,0		; Mostra o caractereque estava na posição do AVATAR
+			mov		ah, 02h		; IMPRIME caracter da posição no canto
+			mov		dl, Car	
+			int		21H			
+		
+			goto_xy	POSx,POSy	; Vai para posição do cursor
+	IMPRIME:	
+			; mov		ah, 02h
+			; mov		dl, 190		; Coloca AVATAR
+			; int		21H	
+			; goto_xy	POSx,POSy	; Vai para posição do cursor
+			
+			; mov		al, POSx	; Guarda a posição do cursor	
+			; mov		POSxa, al
+			; mov		al, POSy	; Guarda a posição do cursor
+			; mov 	POSya, al
+			
+	LER_SETA:	
+			call 	ReadKeyboardInput
+			cmp		ah, 1
+			je		ESTEND
+			
+			CMP 	AL, 27	; ESCAPE
+			JE		FIM
+			CMP		AL, 13
+			je		ASSINALA
+			jmp		LER_SETA
+			
+	ESTEND:	cmp 	al,48h
+			jne		BAIXO
+			dec		POSy		;cima
+			jmp		CICLO
+
+	BAIXO:	cmp		al,50h
+			jne		ESQUERDA
+			inc 	POSy		;Baixo
+			jmp		CICLO
+
+	ESQUERDA:
+			cmp		al,4Bh
+			jne		DIREITA
+			dec		POSx		;Esquerda
+			dec		POSx		;Esquerda
+			jmp		CICLO
+
+	DIREITA:
+			cmp		al,4Dh
+			jne		LER_SETA 
+			inc		POSx		;Direita
+			inc		POSx		;Direita
+			jmp		CICLO
+
+					; INT 10,9 - Write Character and Attribute at Cursor Position
+					; AH = 09
+					; AL = ASCII character to write
+					; BH = display page  (or mode 13h, background pixel value)
+					; BL = character attribute (text) foreground color (graphics)
+					; CX = count of characters to write (CX >= 1)
+	ASSINALA:
+			mov		bl, cor
+			not		bl
+			mov		cor, bl
+			mov 	ah, 09h
+			mov		al, car
+			mov		bh, 0
+			mov		cx, 1
+			int		10h
+			jmp		CICLO
+	fim:	
+			RET
+HandleWordSelection	endp
+
+HandleGame proc
+		call 	CleanScreen
+		goto_xy	0,0
+		call	DisplayFile
+		call	HandleWordSelection
+		goto_xy	0,22
+HandleGame	endp
 
 GameMenu proc
 	loopMenu:
@@ -91,7 +253,7 @@ GameMenu proc
 		; based on the user key input call the respective procedure
 
 		cmp 	al, 49 ; 1
-		je		OPCLEAVE
+		je		OPCSTARTGAME
 		cmp		al, 50 ; 2
 		je		OPCLEAVE
 		cmp		al, 51 ; 3
@@ -102,6 +264,8 @@ GameMenu proc
 		je		OPCLEAVE
 		jmp     loopMenu ; try again
 
+		OPCSTARTGAME:
+			call HandleGame
 		OPCLEAVE:
 			mov	ah,4CH
 			INT	21H
@@ -115,9 +279,6 @@ Main	proc
 	mov			ds,ax
 	mov			ax,0B800h
 	mov			es,ax
-
-	;call CleanScreen		; clean the game screen
-	;call		DisplayMenu     ; display the game menu
 	call GameMenu ; start the game
 
 Main	endp 					; fim do main
@@ -130,7 +291,3 @@ end		Main 					; fim do programa
 ; ------------------------------------------------------------------
 ; ..------------------------ END OF MAIN ---------------------------
 ; ------------------------------------------------------------------
-
-
-; ============= END OF MAIN PROCEDURES =============
-
